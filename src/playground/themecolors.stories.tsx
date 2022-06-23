306e17710df0e33601d00c6cc41d41b06842d44e
yves <misterybeer@gmail.com>
2022-06-23 15:38:54 +0200

    copy to clipboard option in colors

--- src/playground/themecolors.stories.tsx
+++ src/playground/themecolors.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Provider } from '~'
 import { Text } from '~/components/Text'
 import { Color } from '~/types'
@@ -9,8 +9,6 @@ import { theme } from '../themes/light'
 import { useHover } from '~/hooks'
 
 const ColorArr = theme.colors
-
-const hover = false
 
 const HexCode = (color) => {
   const rgba = color.replace(/^rgba?\(|\s+|\)$/g, '').split(',')
@@ -50,81 +48,68 @@ export const ThemeColors = () => {
               marginBottom: 20,
             }}
           >
-            <div
-              style={{
-                width: 200,
-                height: 100,
-                backgroundColor: color(val),
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-              }}
-            >
-              <div
-                onClick={() => {
-                  navigator.clipboard.writeText(
-                    HexCode('rgba(' + ColorArr[val].join() + ')')
-                  )
-                }}
-              >
-                {hover ? (
-                  <Text size="sm">
-                    <DuplicateIcon
-                      size={16}
-                      style={{ display: 'inline-block' }}
-                    />{' '}
-                    Copy
-                  </Text>
-                ) : (
-                  <Text size="sm">
-                    {HexCode('rgba(' + ColorArr[val].join() + ')')}
-                  </Text>
-                )}
-              </div>
-
-              <div
-                onClick={() => {
-                  navigator.clipboard.writeText(
-                    'rgba(' + ColorArr[val].join() + ')'
-                  )
-                }}
-              >
-                {hover ? (
-                  <Text>
-                    <DuplicateIcon
-                      size={16}
-                      style={{ display: 'inline-block' }}
-                    />{' '}
-                    Copy
-                  </Text>
-                ) : (
-                  <Text>rgba({ColorArr[val].join()})</Text>
-                )}
-              </div>
-            </div>
-            <div
-              onClick={() => {
-                navigator.clipboard.writeText(val)
-              }}
-            >
-              {hover ? (
-                <Text weight={500} size="sm" style={{ margin: 10 }}>
-                  <DuplicateIcon
-                    size={16}
-                    style={{ display: 'inline-block' }}
-                  />{' '}
-                  Copy
-                </Text>
-              ) : (
-                <Text weight={500} size="sm" style={{ margin: 10 }}>
-                  {val}
-                </Text>
-              )}
-            </div>
+            <ColorBlock val={val} />
           </div>
         ))}
       </div>
     </Provider>
+  )
+}
+
+const ColorBlock = ({ val }) => {
+  const { listeners, hover } = useHover()
+  const [copied, setCopied] = useState(false)
+  return (
+    <>
+      <div
+        style={{
+          width: 200,
+          height: 100,
+          backgroundColor: color(val),
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <div>
+          <Text size="sm">{HexCode('rgba(' + ColorArr[val].join() + ')')}</Text>
+        </div>
+
+        <div
+          onClick={() => {
+            navigator.clipboard.writeText('rgba(' + ColorArr[val].join() + ')')
+          }}
+        >
+          <Text>rgba({ColorArr[val].join()})</Text>
+        </div>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          display: 'block',
+          boxSizing: 'border-box',
+          height: 32,
+          overflow: 'hidden',
+          cursor: 'pointer',
+        }}
+        onClick={() => {
+          navigator.clipboard.writeText(val)
+          setCopied(true)
+        }}
+        {...listeners}
+      >
+        {hover ? (
+          <Text weight={500} size="sm" style={{ margin: 10 }}>
+            <DuplicateIcon size={16} style={{ display: 'inline-block' }} />
+            {copied ? ' Copied' : ' Copy'}
+          </Text>
+        ) : (
+          <Text weight={500} size="sm" style={{ margin: 10 }}>
+            {val}
+          </Text>
+        )}
+      </div>
+    </>
   )
 }
