dbfb2e378f58186c91c49ef5e6285f5036ac7bc6
youzi <youri@saulx.com>
2022-06-27 10:24:33 +0200

    fix space util

--- src/utils/spaceToPx.ts
+++ src/utils/spaceToPx.ts
@@ -2,6 +2,6 @@ import type { Space } from '~/types'
 
 export const spaceToPx = (space?: Space, defaultValue = 24) => {
   if (space) {
-    return space === true ? defaultValue : Number(space)
+    return space === true ? defaultValue : parseInt(space as string)
   }
 }
