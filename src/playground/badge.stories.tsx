8d6d3c6c3bddb9f50a91f619f7e697e6649fef5c
yves <misterybeer@gmail.com>
2022-06-28 11:16:03 +0200

    Badges of honor

--- src/playground/badge.stories.tsx
+++ src/playground/badge.stories.tsx
@@ -8,27 +8,53 @@ export const Badges = () => {
     <Provider>
       <Badge>Badge</Badge>
       <br />
-      <Badge iconLeft={AddIcon}>Icon Left</Badge>
+      <Badge light>Light</Badge>
       <br />
-      <Badge
-        iconLeft={
-          <DotIcon
-            color="AccentForestgreen"
-            size={10}
-            style={{ marginRight: 10 }}
-          />
-        }
-      >
+      <Badge ghost>Ghost</Badge>
+      <br />
+      <Badge ghost outline>
+        Ghost Outline
+      </Badge>
+      <br />
+
+      <Badge iconLeft={<DotIcon color="AccentForestgreen" size={10} />}>
         Icon Left
       </Badge>
       <br />
-      <Badge iconRight={CloseIcon}>Icon Right</Badge>
+      <Badge iconRight={<CloseIcon color="Background0dp" />}>Icon Right</Badge>
       <br />
-      <Badge light>Light</Badge>
-      <br />
+
       <Badge outline>Outline</Badge>
       <br />
       <Badge boxed>Boxed</Badge>
+      <br />
+      <Badge boxed outline ghost>
+        Boxed
+      </Badge>
+      <br />
+      <Badge outline ghost iconLeft={<DotIcon size={10} />}>
+        outline ghost iconleft
+      </Badge>
+      <br />
+      <Badge
+        boxed
+        outline
+        ghost
+        iconRight={<DotIcon size={10} color="AccentBrightpurple" />}
+      >
+        Boxed
+      </Badge>
+      <br />
+      <Badge action>Action</Badge>
+      <br />
+      <Badge action light>
+        Action Light
+      </Badge>
+      <br />
+      <Badge action light boxed outline>
+        Action Light boxed outline
+      </Badge>
+      <br />
     </Provider>
   )
 }
