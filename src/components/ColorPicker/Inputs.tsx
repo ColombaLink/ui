9f1b8401e5ff249a3d9730ea68e98bf7e423095d
youzi <youri@saulx.com>
2022-07-08 09:50:20 +0200

    better hex input

--- src/components/ColorPicker/Inputs.tsx
+++ src/components/ColorPicker/Inputs.tsx
@@ -1,52 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from '~'
 import { minmax, toHex } from './utils'
 
-export const Inputs = ({ rgb, alpha, onRgbChange, onAlphaChange }) => {
-  const [r, g, b] = rgb
-  const margin = { marginRight: 8 }
-  const [hex, setHex] = useState(
-    `${toHex(r)}${toHex(g)}${toHex(b)}`.toUpperCase()
+const HexInput = ({ r, g, b, onRgbChange }) => {
+  return (
+    <Input
+      value={`${toHex(r)}${toHex(g)}${toHex(b)}`.toUpperCase()}
+      suggest={(v) => v.padEnd(6, '0')}
+      transform={(v) => v.toUpperCase().replace(/[^A-F0-9]/g, '')}
+      forceSuggestion
+      noInterrupt
+      onChange={(v) => {
+        const str = v.padEnd(6, '0')
+        const r = parseInt(str.substring(0, 2), 16)
+        const g = parseInt(str.substring(2, 4), 16)
+        const b = parseInt(str.substring(4, 6), 16)
+
+        if (r <= 255 && g <= 255 && b <= 255) {
+          onRgbChange([r, g, b])
+        }
+      }}
+    />
   )
+}
+
+export const Inputs = ({ rgb, alpha, onRgbChange, onAlphaChange }) => {
+  let [r, g, b] = rgb
+  const margin = { marginLeft: 8 }
+
+  r = Math.round(r)
+  b = Math.round(b)
+  g = Math.round(g)
+
   return (
     <div style={{ display: 'flex' }}>
-      <Input
-        style={margin}
-        value={hex}
-        suggest={(v) => {
-          return v.padEnd(6, '0')
-        }}
-        onChange={(v) => {
-          const str = v.padEnd(6, '0')
-          const r = parseInt(str.substring(0, 2), 16)
-          const g = parseInt(str.substring(2, 4), 16)
-          const b = parseInt(str.substring(4, 6), 16)
-
-          if (r <= 255 && g <= 255 && b <= 255) {
-            onRgbChange([r, g, b])
-            setHex(v.toUpperCase())
-          }
-        }}
-      />
+      <HexInput r={r} g={g} b={b} onRgbChange={onRgbChange} />
       <Input
         style={margin}
         type="number"
-        value={Math.round(r)}
+        value={r}
         onChange={(r) => onRgbChange([minmax(0, r, 255), g, b])}
       />
       <Input
         style={margin}
         type="number"
-        value={Math.round(g)}
+        value={g}
         onChange={(g) => onRgbChange([r, minmax(0, g, 255), b])}
       />
       <Input
         style={margin}
         type="number"
-        value={Math.round(b)}
+        value={b}
         onChange={(b) => onRgbChange([r, g, minmax(0, b, 255)])}
       />
-      <Input type="number" value={alpha} onChange={onAlphaChange} />
+      <Input
+        style={margin}
+        type="number"
+        value={alpha}
+        onChange={onAlphaChange}
+      />
     </div>
   )
 }
