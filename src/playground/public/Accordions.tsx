6b1563a8f66b30b934fe0a02d2faa20156e972d2
yves <misterybeer@gmail.com>
2022-07-11 12:22:24 +0200

    com

--- src/playground/public/Accordions.tsx
+++ src/playground/public/Accordions.tsx
@@ -1,7 +1,38 @@
 import React from 'react'
-import { Accordion } from '../../'
+import { Accordion, AccordionItem, Text } from '../../'
 import ComponentViewer from '../ComponentViewer'
 
 export const Accordions = () => {
-  return <ComponentViewer component={Accordion} />
+  const accordionCodeExample = `import { Accordion, AccordionItem, Text } from '@based/ui
+
+  const Accordion = () => {}
+ 
+  <Accordion>   
+    <AccordionItem title="Bonjour"><Text>Bonjour</Text></AccordionItem>
+    <AccordionItem title="Bonsoir"><Text>Bonsoir</Text></AccordionItem>
+    <AccordionItem title="Bonne nuit"><Text>Bonne Nuit</Text></AccordionItem>
+  </Accordion>    
+  `
+
+  return (
+    <ComponentViewer
+      component={Accordion}
+      examples={[
+        {
+          props: {
+            children: (
+              <AccordionItem title="Accordion item title">
+                <Text>Bonjour</Text>
+              </AccordionItem>
+            ),
+          },
+        },
+        {
+          props: {
+            children: accordionCodeExample,
+          },
+        },
+      ]}
+    />
+  )
 }
