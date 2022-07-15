b059cef81bc186e2a8a78d802cffab76008db978
yves <misterybeer@gmail.com>
2022-07-15 13:45:29 +0200

    More Drag and drop

--- src/playground/public/Lists.tsx
+++ src/playground/public/Lists.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
-import { DraggableListWrapper, ListItem } from '~/components/ListItems'
+import React, { useState } from 'react'
 import { DragDropIcon } from '~/icons'
 import { Text } from '~/components/Text'
 
@@ -11,15 +10,12 @@ export const Lists = () => {
   const [mouseOverListItem, setMouseOverListItem] = useState(null)
 
   const dragHandler = (e) => {
-    console.log('drag')
-    console.log('Active dragging:', e.target.id)
     setActiveListItem(+e.target.id)
   }
 
   const dragOverHandler = (e) => {
     // moet deze zetten zodat ze gedropt kunnen worden (animatie'tje)
     e.preventDefault()
-    console.log('draggin over:', e.target.id)
     setMouseOverListItem(+e.target.id)
   }
 
@@ -27,17 +23,10 @@ export const Lists = () => {
     if (activeListItem === mouseOverListItem) {
       console.log('Test  = Same item, nothing happens')
     }
-
     if (activeListItem !== mouseOverListItem) {
-      e.preventDefault()
-      e.stopPropagation()
       arrayMagic(activeListItem, mouseOverListItem, list)
     }
   }
-
-  // const dropHandler = (e) => {
-  //   console.log('dropped')
-  // }
 
   // some array magic slicing etc
   const arrayMagic = (activeItem, mouseOverItem, list) => {
@@ -47,7 +36,6 @@ export const Lists = () => {
 
     //remove the old active item from the list
     newList.splice(activeListItem, 1)
-
     //put old item back in the list
     newList.splice(mouseOverItem, 0, oldItem)
 
@@ -73,15 +61,9 @@ export const Lists = () => {
             alignItems: 'center',
             marginBottom: '4px',
           }}
-          onDrag={(e) => {
-            dragHandler(e)
-          }}
-          onDragOver={(e) => {
-            dragOverHandler(e)
-          }}
-          onDragEnd={(e) => {
-            dragEndHandler(e)
-          }}
+          onDrag={(e) => dragHandler(e)}
+          onDragOver={(e) => dragOverHandler(e)}
+          onDragEnd={(e) => dragEndHandler(e)}
         >
           <DragDropIcon style={{ marginRight: 12, pointerEvents: 'none' }} />
           {item}
@@ -89,22 +71,6 @@ export const Lists = () => {
       ))}
 
       <br />
-      <Text>Try another one</Text>
-      <br />
-
-      {/* {list.map((item, index) => (
-        <ListItem
-          draggable
-          key={index}
-          id={`${index}`}
-          left={<Text>{item}</Text>}
-          // @ts-ignore
-          onMouseDown={(e) => mouseDownHandler(e)}
-          onMouseOver={(e) => mouseOverHandler(e)}
-          onMouseUp={mouseUpHandler}
-          onMouseMove={mouseMoveHandler}
-        />
-      ))} */}
     </div>
   )
 }
