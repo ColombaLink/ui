dfef55c974345540947a1c3afd3607e4d2b22b4a
yves <misterybeer@gmail.com>
2022-08-02 17:04:54 +0200

    footerbottom WIP

---
+++ src/components/Flow/FooterBottom.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+
+type FooterBottomProps = {
+  context: any
+  seqItems: any
+  isDragOver: any
+  isDragOverSeq: any
+  isDropLoading: any
+  index: any
+  wrappedData: any
+}
+
+export const FooterBottom: FC<FooterBottomProps> = ({
+  context,
+  seqItems,
+  isDragOver,
+  isDragOverSeq,
+  isDropLoading,
+  index,
+  wrappedData,
+}) => {
+  return <></>
+}
