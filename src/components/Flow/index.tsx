5af78f21b1d511335b120be3329d1a9f423ff3d1
yves <misterybeer@gmail.com>
2022-08-03 12:21:53 +0200

    something wrong in useSelect ?!?

--- src/components/Flow/index.tsx
+++ src/components/Flow/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import AutoSizer from 'react-virtualized-auto-sizer'
+import { VariableSizeList } from 'react-window'
 import { Footer } from './Footer'
 import { FooterBottom } from './FooterBottom'
 import { Sequence } from './Sequence'
 import { Header } from './Header'
 import { wait } from '@saulx/utils'
 import { AddIcon } from '~'
+import { getData } from './getData'
+import useDragScroll from '~/hooks/useDragScroll'
 
 // flow props types
 
@@ -13,20 +16,114 @@ type FlowProps = {}
 
 //header flow component
 
-//footer flow component
+const defaultItemProps = {
+  title: { path: ['title'] },
+  items: { path: ['items'] },
+}
 
 export const Flow: FlowProps = (props) => {
+  const { items = [], footer, paddingTop = 0, paddingBottom = 0 } = props
+  const autoFocusRef = useRef()
+  const itemsWithNew = footer
+    ? [
+        ...items,
+        {
+          '@@newSequence': true,
+        },
+      ]
+    : items
+
+  const listRef = useRef<any>()
+
+  useEffect(() => {
+    if (listRef.current) {
+      listRef.current.resetAfterIndex(0)
+    }
+  }, [items])
+
+  const expandedRef = useRef({ cnt: (~~(1000 * Math.random())).toString(16) })
+
+  const updateExpandList = (action) => {
+    expandedRef.current[action] = !expandedRef.current[action]
+    listRef.current.resetAfterIndex(action)
+  }
+
+  const expanded = expandedRef.current
   return (
     <AutoSizer>
-      <Header outline label="Editable label" onEditTitle={() => {}} />
-      <Footer
-        label="New Footer seq"
-        onClick={async (e, data) => {
-          await wait(1e3)
-        }}
-        outline
-        icon={AddIcon}
-      />
+      {({ height, width }) => {
+        return (
+          <VariableSizeList
+            ref={listRef}
+            width={props.width || width}
+            style={{
+              paddingTop,
+              paddingBottom,
+              overflowX: 'hidden',
+            }}
+            itemCount={itemsWithNew.length}
+            height={height}
+            itemData={{
+              items: itemsWithNew,
+              context: {
+                autoFocusRef,
+                showIndex: true,
+                ...props,
+                updateExpandList,
+                expanded,
+                seqItemProps: props.itemProps,
+                itemProps: props.itemProps
+                  ? props.itemProps.items
+                    ? props.itemProps.items.props
+                    : undefined
+                  : undefined,
+              },
+              width,
+            }}
+            itemSize={(index) => {
+              let x = 48 + 35
+              if (index === 0 && paddingTop) {
+                x += paddingTop
+              }
+
+              const data = itemsWithNew[index]
+
+              if (index === itemsWithNew.length - 1) {
+                x += paddingBottom
+              }
+
+              if (data['@@newSequence']) {
+                return x
+              }
+
+              if (props.stepFooter) {
+                x += 48
+              }
+
+              if (props.sequenceSpacing) {
+                x += props.sequenceSpacing
+              }
+
+              const selectItems =
+                (props.itemProps && props.itemProps.items) ||
+                defaultItemProps.items
+
+              const items = getData(data, selectItems.path)
+
+              if (
+                !props.expandable ||
+                (props.defaultIsExpanded ? !expanded[index] : expanded[index])
+              ) {
+                return (items ? items.length : 0) * 48 + x
+              }
+              return x
+            }}
+            {...useDragScroll(true)}
+          >
+            {Sequence}
+          </VariableSizeList>
+        )
+      }}
     </AutoSizer>
   )
 }
--- src/playground/public/FlowSequences.tsx
+++ src/playground/public/FlowSequences.tsx
@@ -1,11 +1,180 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import { Flow } from '~/components/Flow'
+import { wait } from '@saulx/utils'
+import { Button } from '~/components/Button'
+import { useOverlay } from '~/hooks'
 
 export const FlowSequences = () => {
+  const profilePic = 'https://scx2.b-cdn.net/gfx/news/hires/2019/2-forest.jpg'
+
+  const randomDate = () => {
+    const start = new Date()
+    const end = new Date()
+    return new Date(
+      start.getTime() + Math.random() * (end.getTime() - start.getTime())
+    ).getTime()
+  }
+
+  const listData = []
+  for (let i = 0; i < 50; i++) {
+    listData.push({
+      id: i,
+      text: 'Item ' + i,
+      longtext: 'test',
+      flurpen: 'testicon',
+      img: i !== 3 ? profilePic : '',
+      created: randomDate(),
+    })
+  }
+
+  const modalsnurp = (update) => {
+    return useOverlay(
+      ({ data }) => {
+        console.info(data)
+        return (
+          <div>
+            <Button
+              onClick={() => {
+                data.text = 'blah'
+                update()
+              }}
+            >
+              MAKE UPDATE
+            </Button>
+          </div>
+        )
+      },
+      {
+        header: ({ data }) => {
+          return {
+            title: data.text,
+          }
+        },
+      }
+    )
+  }
+
+  const [, update] = useReducer((x) => x + 1, 1)
+  const fd = [...listData]
+
   return (
     <div>
       Flow here
-      <Flow />
+      <Flow
+        defaultIsExpanded
+        expandable
+        itemProps={{
+          id: ['id'],
+          items: {
+            props: {
+              title: { path: ['text'] },
+              icon: { path: ['flurpen'], framed: true },
+              id: ['id'],
+            },
+            path: ['items'],
+          },
+        }}
+        items={[
+          {
+            title: 'my seq0',
+            id: 0,
+            items: [],
+          },
+          {
+            title: 'my seq1',
+            id: 1,
+            items: [
+              {
+                flurpen: {
+                  name: 'Logic',
+                  color: { color: 'secondary' },
+                },
+                text: 'yesh',
+                id: 1,
+              },
+            ],
+          },
+          {
+            title: 'my seq2',
+            id: 2,
+            items: fd.slice(0, 10),
+          },
+          {
+            title: 'my seq3',
+            id: 3,
+            items: fd.slice(10),
+          },
+        ]}
+        paddingLeft={200}
+        paddingTop={30}
+        paddingBottom={30}
+        paddingRight={200}
+        onDrop={async (e, data) => {
+          if (data.data) {
+            console.info(data)
+            data.data.sort((a, b) => (a.index > b.index ? 1 : -1))
+            for (const d of data.data) {
+              listData.splice(
+                listData.findIndex((x) => d.data.id === x.id),
+                1
+              )
+            }
+            await wait(1e3)
+            update()
+          }
+        }}
+        header={{
+          onEditTitle: () => {},
+          children: () => {
+            // yes
+            return <>blah</>
+          },
+        }}
+        onDropSequence={async (e, data) => {
+          console.info('SEQ AREA', data)
+          await wait(1e3)
+        }}
+        footer={{
+          label: 'New sequence',
+          onClick: async (e, data) => {
+            await wait(1e3)
+          },
+        }}
+        stepFooter={{
+          label: 'New step',
+          onClick: async (e, data) => {
+            listData.unshift({
+              id: listData.length,
+              text: 'gruken ' + listData.length,
+            })
+            await wait(1e3)
+            update()
+          },
+        }}
+        onClick={modalsnurp(update)}
+      >
+        {({ isHover, data, items }) => {
+          return (
+            <div
+              style={{
+                width: 50,
+                height: 24,
+                position: 'relative',
+              }}
+            >
+              {data.data.id === 2 ? (
+                <div
+                  style={{
+                    position: 'absolute',
+                    left: 26.5 + 25,
+                    top: 0,
+                  }}
+                ></div>
+              ) : null}
+            </div>
+          )
+        }}
+      </Flow>
     </div>
   )
 }
