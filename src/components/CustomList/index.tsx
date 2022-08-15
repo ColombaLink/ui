4993f4f310f35485f3af13610f18e5000aca27a3
yves <misterybeer@gmail.com>
2022-08-15 18:02:46 +0200

    eventlistener render bug ??

--- src/components/CustomList/index.tsx
+++ src/components/CustomList/index.tsx
@@ -1,10 +1,10 @@
 import React, {
-  ReactNode,
   CSSProperties,
   FC,
   useRef,
   useState,
   Ref,
+  useEffect,
 } from 'react'
 import AutoSizer from 'react-virtualized-auto-sizer'
 import { SortableFixedSizeList, ChildrenProps } from 'react-window-sortable'
@@ -20,7 +20,6 @@ type CustomListProps = {
   items?: any[]
   draggable?: boolean
   expandable?: boolean
-  children?: ReactNode
   itemSize?: number
   itemSpace?: Space
   style?: CSSProperties
@@ -30,13 +29,11 @@ export const CustomList: FC<CustomListProps> = ({
   items,
   draggable,
   expandable,
-  children,
   itemSpace = 0,
   itemSize = 56 + +itemSpace,
   style,
 }) => {
   const [data, setData] = useState(items)
-  const [draggy, setDraggy] = useState(false)
 
   const listRef = useRef<any>()
 
@@ -62,32 +59,34 @@ export const CustomList: FC<CustomListProps> = ({
     },
   })
 
+  // Whats going on here ?
+
   const sillyDiv = document.createElement('div')
   sillyDiv.setAttribute('id', 'sillyDivId')
   sillyDiv.innerHTML = 'hallo'
   sillyDiv.style.width = '100px'
   sillyDiv.style.height = '60px'
   sillyDiv.style.background = 'red'
-  sillyDiv.style.zIndex = '9999'
 
   const addDivToCursor = (e) => {
     console.log(e.target)
 
-    document.addEventListener('mousedown', function () {
-      document.addEventListener('mousemove', movingmouse)
+    document.addEventListener('mousedown', () => {
+      document.addEventListener('mousemove', movingMouse)
 
-      document.body.appendChild(sillyDiv)
-
-      function movingmouse(e) {
+      function movingMouse(e) {
+        document.body.appendChild(sillyDiv)
         sillyDiv.style.position = 'absolute'
         sillyDiv.style.left = `${e.clientX}px`
         sillyDiv.style.top = `${e.clientY}px`
       }
 
-      document.addEventListener('mouseup', function () {
+      document.addEventListener('mouseup', () => {
+        console.log('mouse is up')
+
+        document.removeEventListener('mousemove', movingMouse)
+        document.removeEventListener('mousedown', addDivToCursor)
         document.getElementById('sillyDivId').remove()
-        //    document.body.removeChild(sillyDiv)
-        document.removeEventListener('mousemove', movingmouse)
       })
     })
   }
@@ -127,14 +126,15 @@ export const CustomList: FC<CustomListProps> = ({
                       <DragDropper
                         onMouseDown={(e) => {
                           onSortMouseDown(e)
-                          // append a copy to the mouse cursor
                           addDivToCursor(e)
+                        }}
+                        onMouseUp={(e) => {
+                          console.log('mouse up')
                         }}
                         style={{ marginLeft: 16, marginRight: 16 }}
                       />
                     )}
                     {data[index]}
-
                     <More
                       onClick={useContextMenu(
                         SimpleMenu,
