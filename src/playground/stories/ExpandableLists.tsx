4192b614701c38f68aac4441ba0f1c17fd250b56
yves <misterybeer@gmail.com>
2022-08-22 13:48:03 +0200

    WIP

--- src/playground/stories/ExpandableLists.tsx
+++ src/playground/stories/ExpandableLists.tsx
@@ -2,6 +2,39 @@ import React from 'react'
 import { ExpandableList } from '~'
 
 export const ExpandableLists = () => {
+  // Example Data
+  const drillDownData = []
+  for (let i = 0; i < 12; i++) {
+    const d = []
+    for (let i = 0; i < 10; i++) {
+      d.push({
+        title: 'Yesh nested ' + i,
+        value: ~~(Math.random() * 1e6),
+        items: [
+          {
+            title: 'more nested ' + i,
+            value: 10,
+          },
+        ],
+      })
+    }
+    drillDownData.push({
+      title: 'Snurpies ' + i,
+      items: d,
+      value: ~~(Math.random() * 1e6),
+    })
+  }
+
+  //     <ResultList
+  //     items={drillDownData}
+  //     itemProps={{
+  //       id: ['id'],
+  //       title: { path: ['title'] },
+  //     }}
+  //     label="Locations"
+  //     value="Responses"
+  //     />
+
   return (
     <div>
       blaafeah
