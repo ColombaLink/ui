1e0b962f381a0134c0a34fe3218c74ef44b7410d
yves <misterybeer@gmail.com>
2022-09-29 13:12:12 +0200

    hey you what you gonna do, cleanup crew

--- src/components/ResultCard/index.tsx
+++ src/components/ResultCard/index.tsx
@@ -8,7 +8,7 @@ type ResultCardProps = {
   label?: string
   value?: number | { [key: string]: number | string }
   style?: CSSProperties
-  format?: NumberFormat | string | number
+  format?: NumberFormat
   space?: Space
 }
 
@@ -21,6 +21,7 @@ export const ResultCard: FC<ResultCardProps> = ({
   ...props
 }) => {
   if (typeof value === 'object') {
+    // @ts-ignore
     format = value.format
     value = value.value
   }
@@ -45,6 +46,7 @@ export const ResultCard: FC<ResultCardProps> = ({
       <Text weight={400} space="8px">
         {label}
       </Text>
+
       <Text size="24px" weight={700}>
         {prettyNumber(value, format)}
       </Text>
