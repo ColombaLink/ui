d05701b65b92e1429879c6d6652e4fecc3c8db79
yves <misterybeer@gmail.com>
2022-12-22 17:02:42 +0100

    Checkbox fun indeterminate

--- src/playground/stories/Checkboxes.tsx
+++ src/playground/stories/Checkboxes.tsx
@@ -18,6 +18,7 @@ export const Checkboxes = () => {
             label: 'Checkbox checked',
             checked: true,
             color: 'green',
+            indeterminate: true,
           },
         },
         {
@@ -25,6 +26,7 @@ export const Checkboxes = () => {
             label: 'Check it out',
             description: 'Checkbox description',
             checked: true,
+            size: 'sm',
           },
         },
         {
