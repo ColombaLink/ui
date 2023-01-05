2be5a184568130c7a161403116ebeadb514ed896
Jim de Beer <beerdejim@gmail.com>
2023-01-05 14:46:17 +0100

    reenable query update gaurd

--- src/hooks/location/RouteParams.ts
+++ src/hooks/location/RouteParams.ts
@@ -122,13 +122,13 @@ export class RouteParams {
       return true
     }
 
-    // if (this.parsedPath.length && this.ctx.pathChanged) {
-    const nParams = parseRoute(this.ctx, this.parsedPath, this.start)
-    if (!deepEqual(this._pathParams, nParams)) {
-      this._pathParams = nParams
-      return true
+    if (this.parsedPath.length && this.ctx.pathChanged) {
+      const nParams = parseRoute(this.ctx, this.parsedPath, this.start)
+      if (!deepEqual(this._pathParams, nParams)) {
+        this._pathParams = nParams
+        return true
+      }
     }
-    // }
 
     if (this._usesQuery && this.ctx.queryChanged) {
       return true
