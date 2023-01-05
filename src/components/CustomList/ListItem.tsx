df5dd8aafee230fca96d08cbd5138495831d0297
yves <misterybeer@gmail.com>
2023-01-05 14:42:06 +0100

    dgra drop

--- src/components/CustomList/ListItem.tsx
+++ src/components/CustomList/ListItem.tsx
@@ -1,20 +1,78 @@
-import React, { CSSProperties } from 'react'
+import React, { CSSProperties, useRef, useCallback } from 'react'
+import useMultipleEvents from '~/hooks/useMultipleEvents'
+import { useDrag, useDrop } from '~/hooks'
+import { useSelect, useClick } from './hooks/useSelect'
 
 type ListItemProps = {
   index?: number
-
+  data?: any
   style?: CSSProperties
 }
 
-export const ListItem = ({ index, style }: ListItemProps) => {
+export const ListItem = ({
+  data: { items, context },
+  index,
+  style,
+}: ListItemProps) => {
+  let {
+    activeId,
+    onDrop,
+    exportData,
+    draggable = true,
+    showIndex,
+    isActive: isActiveFn,
+  } = context
+  console.log('data', context)
+
+  const ref = useRef<any>()
+
+  const itemData = items[index]
+
+  const wrappedData = {
+    index,
+    data: itemData,
+    exportData,
+  }
+
+  const [drag, isDragging] = draggable ? useDrag(wrappedData, ref) : [{}, false]
+  const [select, isSelected] = useSelect(wrappedData)
+  const [drop, isDragOver, isDropLoading] = useDrop(
+    useCallback(
+      (e, { files, data }) => {
+        if (onDrop) {
+          if (data && data.length) {
+            return onDrop(e, {
+              targetIndex: index,
+              data,
+            })
+          } else if (files) {
+            return onDrop(e, { files, targetIndex: index })
+          }
+        }
+      },
+      [index, items]
+    ),
+    { readFiles: true }
+  )
+
+  console.log('isSelect', isSelected)
+
   return (
     <div
+      ref={ref}
       style={{
-        backgroundColor: index % 2 === 0 ? 'lightblue' : '#f6f6f6',
+        backgroundColor: isSelected
+          ? 'orange'
+          : isDragging
+          ? 'lightgreen'
+          : index % 2 === 0
+          ? 'lightblue'
+          : '#f6f6f6',
         ...style,
       }}
+      {...useMultipleEvents(drag, select)}
     >
-      testing item {index}
+      testing item {items[index]?.text} - {index}
     </div>
   )
 }
