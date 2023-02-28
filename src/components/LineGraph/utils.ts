7a8986ff316eb7fff1a3c680ae6af9fb679933fc
Jim de Beer <beerdejim@gmail.com>
2023-02-28 14:11:56 +0100

    Revert "Merge branch 'main' of github.com:atelier-saulx/ui"
    This reverts commit 273ec0b6833bef48bf17ef6eef37fb04a01e4c96, reversing
    changes made to 4ad5bb10e4b7d22a91c6ea7e8f21743370bd8b9a.

--- src/components/LineGraph/utils.ts
+++ src/components/LineGraph/utils.ts
@@ -1,47 +1,137 @@
-export const averageOrAddData = (data, width, spread) => {
-  const newData: { x: number; y: number }[] = []
-  let stepSize = width / (data.length - 1)
-  if (!spread && stepSize < 10) {
-    const dX = 10 / stepSize
-    const condenseAmount = Math.round(dX)
-    stepSize = width / (Math.floor(data.length / condenseAmount) - 1)
+import { LineGraphData, LineGraphDataInput, Point } from './types'
 
-    for (let i = 0; i < data.length - 1; i += condenseAmount) {
-      let totalX = 0
-      let totalY = 0
-      let pointsTraversed = 0
+export const getMinMax = (data: Point[]) => {
+  let maxY: number
+  let minY: number
+  let maxX: number
+  let minX: number
 
-      for (let j = 0; j < condenseAmount; j++) {
-        if (data[i + j]) {
-          pointsTraversed++
-          totalX += data[i + j].x
-          totalY += data[i + j].y
-        }
-      }
-
-      newData.push({
-        x: totalX / pointsTraversed,
-        y: totalY / pointsTraversed,
-      })
+  for (let i = 0; i < data.length; i++) {
+    const { x, y } = data[i]
+    if (maxY === undefined || y > maxY) {
+      maxY = y
     }
-    data = newData
-  } else if (!spread && stepSize > 20) {
-    const newData = []
-    const len = Math.ceil(width / 20 / data.length)
+    if (minY === undefined || y < minY) {
+      minY = y
+    }
+    if (maxX === undefined || x > maxX) {
+      maxX = x
+    }
+    if (minX === undefined || x < minX) {
+      minX = x
+    }
+  }
+  return { minX, maxX, minY, maxY }
+}
 
-    for (let i = 0; i < data.length - 1; i++) {
-      const next = data[i + 1]
-      const spread = (next.y - data[i].y) / len
-      const xspread = (next.x - data[i].x) / len
-      for (let j = 1; j < len + 1; j++) {
-        newData.push({
-          x: j * xspread + data[i].x,
-          y: j * spread + data[i].y,
-        })
+export const averageData = ({
+  data,
+  lineStepSize,
+  width,
+  targetStepSize = 10,
+}: {
+  data: Point[]
+  lineStepSize: number
+  width: number
+  targetStepSize?: number
+}) => {
+  const dX = targetStepSize / lineStepSize
+  const condenseAmount = Math.round(dX)
+  const newStepSize = width / Math.floor((data.length - 1) / condenseAmount)
+  const newData: Point[] = []
+  const minData: Point[] = []
+  const maxData: Point[] = []
+
+  for (let i = 0; i < data.length; i += condenseAmount) {
+    let totalX = 0
+    let totalY = 0
+    let minY: number
+    let maxY: number
+    let pointsTraversed = 0
+
+    for (let j = 0; j < condenseAmount; j++) {
+      const item = data[i + j]
+      if (item) {
+        pointsTraversed++
+        if (minY === undefined || item.y < minY) minY = item.y
+        if (maxY === undefined || item.y > maxY) maxY = item.y
+        totalX += item.x
+        totalY += item.y
       }
     }
 
-    data = newData
+    newData.push({
+      x: totalX / pointsTraversed,
+      y: totalY / pointsTraversed,
+    })
+    minData.push({
+      x: totalX / pointsTraversed,
+      y: minY,
+    })
+    maxData.push({
+      x: totalX / pointsTraversed,
+      y: maxY,
+    })
+  }
+  return { data: newData, minData, maxData, stepSize: newStepSize }
+}
+
+export const getGlobalMinMax = (data: LineGraphData) => ({
+  globalMinX: Math.min(...Object.values(data).map((l) => l.minX)),
+  globalMaxX: Math.max(...Object.values(data).map((l) => l.maxX)),
+  globalMinY: Math.min(...Object.values(data).map((l) => l.minY)),
+  globalMaxY: Math.max(...Object.values(data).map((l) => l.maxY)),
+})
+
+export const processData = ({
+  dataInput,
+}: {
+  dataInput: LineGraphDataInput
+}) => {
+  // Unify data format
+  let data: LineGraphData
+  if (Array.isArray(dataInput)) {
+    data = { '': { data: dataInput } }
+  } else if (Array.isArray(dataInput[Object.keys(dataInput)[0]])) {
+    data = Object.keys(dataInput).reduce(
+      (newData, key) => ({ ...newData, [key]: { data: dataInput[key] } }),
+      {}
+    )
+  } else {
+    data = dataInput as LineGraphData
+  }
+
+  // calculate mins and maxs
+  for (const key in data) {
+    const { minX, maxX, minY, maxY } = getMinMax(data[key].data)
+    data[key].minX = minX
+    data[key].maxX = maxX
+    data[key].minY = minY
+    data[key].maxY = maxY
   }
   return data
 }
+
+export const generatePoints = ({
+  data,
+  paddingLeft,
+  width,
+  lineStepSize,
+  ySpread,
+  globalMinY,
+  pxValue,
+}: {
+  data: Point[]
+  paddingLeft: number
+  width: number
+  lineStepSize: number
+  ySpread: number
+  globalMinY: number
+  pxValue: number
+}) =>
+  data.map((dataItem, index) => {
+    return {
+      x: paddingLeft * width + lineStepSize * index,
+      y: (ySpread - (dataItem.y - globalMinY)) / pxValue,
+    }
+  })
