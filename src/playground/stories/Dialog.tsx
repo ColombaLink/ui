d8e49bfd3522ee567b33e8c75760153643f2acce
Jim de Beer <beerdejim@gmail.com>
2023-03-22 18:42:58 +0100

    fix

--- src/playground/stories/Dialog.tsx
+++ src/playground/stories/Dialog.tsx
@@ -1,5 +1,33 @@
 import React from 'react'
 import ComponentViewer from '../ComponentViewer'
+import { useRoute, Text, Button, AddIcon, MinusIcon, Dialog as Dc } from '~'
+
+const SomeComponent = () => {
+  const route = useRoute('[x]')
+  return (
+    <Dc label="Label">
+      <Text>This is a path: {route.path.x}</Text>
+      <Dc.Buttons>
+        <Button
+          ghost
+          icon={<MinusIcon />}
+          onClick={() => {
+            route.setPath({ x: Number(route.path.x || 0) - 1 })
+          }}
+        />
+        <Button
+          ghost
+          icon={<AddIcon />}
+          onClick={() => {
+            route.setPath({ x: Number(route.path.x || 0) + 1 })
+          }}
+        />
+      </Dc.Buttons>
+    </Dc>
+  )
+}
+
+global.SomeComponent = SomeComponent
 
 export const Dialog = () => {
   return (
@@ -21,21 +49,14 @@ export const Dialog = () => {
 </Dialog>`,
           },
           {
-            code: `import { Dialog, Text, Button, useDialog } from '@based/ui'
+            code: `import { Dialog, Text, Button, useDialog, useRoute } from '@based/ui'
 
 const dialog = useDialog();
+const route = useRoute('[x]', { x: 1 });
 
-<Button onClick={() => {
-  dialog.open(<Dialog label="Label">
-  <Text space>
-    Do something!
-  </Text>
-  <Dialog.Buttons border>
-    <Dialog.Cancel />
-    <Dialog.Confirm />
-  </Dialog.Buttons>
-</Dialog>)
-}}>Open</Button>
+<Button large onClick={() => dialog.open(<SomeComponent />)}>
+  Open {route.path.x}
+</Button>
 `,
           },
         ]}
