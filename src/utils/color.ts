9b14b46288910bbbcf41a02d9931e75a3dd43eab
Jim de Beer <beerdejim@gmail.com>
2023-05-02 07:46:18 +0200

    fix

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -11,31 +11,41 @@ export type StateColor = `${BaseColor}:${ColorVariant}`
 export type Color = BaseColor | StateColor | cssColorString
 
 type ColorFn = {
-  (name: Color, variant?: ColorVariant, light?: boolean): cssColorString | null
-  (name: Color, light?: boolean, empty?: undefined): cssColorString | null
+  (name: Color, variant?: ColorVariant, light?: boolean):
+    | cssColorString
+    | undefined
+  (name: Color, light?: boolean, empty?: undefined): cssColorString | undefined
 }
 
-export const color: ColorFn = (name, variant, light): cssColorString | null => {
+export const color: ColorFn = (
+  name,
+  variant,
+  light
+): cssColorString | undefined => {
   if (typeof name === 'string') {
     if (name === 'inherit' || name === 'currentColor' || /^var\(/.test(name)) {
       return name as cssColorString
     }
     if (variant === true) {
-      variant = null
+      variant = undefined
       light = true
     }
     if (name in vars) {
       if (variant) {
-        return color(`${name}:${variant}` as Color, null, light)
+        return color(`${name}:${variant}` as Color, undefined, light)
       }
       const key = light && !name.startsWith('light') ? `light${name}` : name
       return vars[key]
     }
     if (name.includes(':')) {
-      return color(name.substring(0, name.indexOf(':')) as Color, null, light)
+      return color(
+        name.substring(0, name.indexOf(':')) as Color,
+        undefined,
+        light
+      )
     }
   }
-  return null
+  return undefined
 }
 
 const colors = Object.keys(baseTheme.colors).filter(
