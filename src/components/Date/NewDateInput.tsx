e157385763626f184b1152b3409d2437719ce592
yves <misterybeer@gmail.com>
2023-06-01 14:54:59 +0200

    let the madness continue

--- src/components/Date/NewDateInput.tsx
+++ src/components/Date/NewDateInput.tsx
@@ -1,125 +1,166 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { styled } from 'inlines'
 import { border, color } from '~/utils'
 import { CalendarAltIcon } from '~/icons'
 
-export const NewDateInput = () => {
+type newDateProps = {
+  // milliseconds
+  value?: number
+  setValue: (e) => void
+}
+
+const stringToMilliseconds = (str: string): number => {
+  const dateString = `${str?.split('/').reverse().join('-')}T00:00`
+  const outputMs = new Date(dateString).getTime()
+  console.log('output in ms 🎉', outputMs)
+  return outputMs
+}
+
+export const NewDateInput = ({ value, setValue }: newDateProps) => {
+  console.log('The value ⏰', value)
+
   const dayRef = useRef(null)
   const monthRef = useRef(null)
   const yearRef = useRef(null)
 
-  const [day, setDay] = useState<any>('')
-  const [month, setMonth] = useState<any>('')
-  const [year, setYear] = useState<any>('')
+  const [day, setDay] = useState<any>(
+    value ? new Date(value).getDate() + 1 : ''
+  )
+  const [month, setMonth] = useState<any>(
+    value ? new Date(value).getMonth() + 1 : ''
+  )
+  const [year, setYear] = useState<any>(
+    value ? new Date(value).getFullYear() : ''
+  )
 
   const [focusField, setFocusField] = useState<
     'dayFocus' | 'monthFocus' | 'yearFocus' | ''
   >('')
 
+  const fullDateString = `${day < 10 ? `0${day}` : day}/${
+    month < 10 ? `0${month}` : month
+  }/${year}`
+
+  useEffect(() => {
+    if (
+      day &&
+      month &&
+      year &&
+      stringToMilliseconds(fullDateString) !== value
+    ) {
+      console.log('FIREBALL 🔥')
+      setValue(stringToMilliseconds(fullDateString))
+    }
+  }, [day, month, year])
+
+  console.log('FullDate Srting', fullDateString)
+
   return (
     <styled.div style={{ marginBottom: 56 }}>
-      <input
-        type="number"
-        style={{ maxWidth: 40, border: '1px solid red' }}
-        ref={dayRef}
-        value={day}
-        onChange={(e) => {
-          if (+e.target.value < 1 || +e.target.value > 31) {
-            setDay(1)
-          } else {
-            setDay(+e.target.value)
-          }
-        }}
-        onKeyDown={(e) => {
-          if (e.key === 'Backspace' && +e.target.value < 10) {
-            setDay('')
-          }
+      <styled.div style={{ opacity: 0.33 }}>
+        <input
+          type="number"
+          style={{ maxWidth: 40, border: '1px solid red' }}
+          ref={dayRef}
+          value={day}
+          onChange={(e) => {
+            if (+e.target.value < 1 || +e.target.value > 31) {
+              setDay(1)
+            } else {
+              setDay(+e.target.value)
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Backspace' && +e.target.value < 10) {
+              setDay('')
+            }
 
-          if (e.key === 'ArrowDown' && +e.target.value === 1) {
-            setDay(32)
-          }
-        }}
-        onKeyUp={(e) => {
-          if (+e.key > 3) {
-            monthRef.current.select()
-          }
-          if (
-            e.target.value.length > 1 &&
-            e.key !== 'ArrowDown' &&
-            e.key !== 'ArrowUp' &&
-            e.key !== 'ArrowLeft'
-          ) {
-            monthRef.current.select()
-          }
-          if (e.key === 'ArrowRight') {
-            // monthRef.current.focus()
-            monthRef.current.select()
-          }
-        }}
-        onFocus={() => setFocusField('dayFocus')}
-        onBlur={() => setFocusField('')}
-      />
-      <input
-        type="number"
-        style={{ maxWidth: 40, border: '1px solid red' }}
-        ref={monthRef}
-        value={month}
-        onChange={(e) => {
-          if (+e.target.value < 1 || +e.target.value > 12) {
-            setMonth(1)
-          } else {
-            setMonth(+e.target.value)
-          }
-        }}
-        onKeyDown={(e) => {
-          if (e.key === 'Backspace' && +e.target.value < 10) {
-            setMonth('')
-          }
-          if (e.key === 'ArrowDown' && +e.target.value === 1) {
-            setMonth(13)
-          }
-        }}
-        onKeyUp={(e) => {
-          if (+e.key > 1 && +e.target.value > 1) {
-            yearRef.current.select()
-          }
-          if (
-            e.target.value.length > 1 &&
-            e.key !== 'ArrowDown' &&
-            e.key !== 'ArrowUp' &&
-            e.key !== 'ArrowLeft'
-          ) {
-            yearRef.current.select()
-          }
-          if (e.key === 'ArrowRight') {
-            yearRef.current.select()
-          }
-          if (e.key === 'ArrowLeft') {
-            dayRef.current.select()
-          }
-        }}
-        onFocus={() => setFocusField('monthFocus')}
-        onBlur={() => setFocusField('')}
-      />
-      <input
-        type="number"
-        style={{ maxWidth: 40, border: '1px solid red' }}
-        ref={yearRef}
-        value={year}
-        onChange={(e) => setYear(+e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === 'Backspace' && e.target.value.length === 1) {
-            setYear('')
-          }
-        }}
-        onKeyUp={(e) => {
-          if (e.key === 'ArrowLeft') {
-            monthRef.current.select()
-          }
-        }}
-        onFocus={() => setFocusField('yearFocus')}
-        onBlur={() => setFocusField('')}
-      />
+            if (e.key === 'ArrowDown' && +e.target.value === 1) {
+              setDay(32)
+            }
+          }}
+          onKeyUp={(e) => {
+            if (+e.key > 3) {
+              monthRef.current.select()
+            }
+            if (
+              e.target.value.length > 1 &&
+              e.key !== 'ArrowDown' &&
+              e.key !== 'ArrowUp' &&
+              e.key !== 'ArrowLeft'
+            ) {
+              monthRef.current.select()
+            }
+            if (e.key === 'ArrowRight') {
+              // monthRef.current.focus()
+              monthRef.current.select()
+            }
+          }}
+          onFocus={() => setFocusField('dayFocus')}
+          onBlur={() => setFocusField('')}
+        />
+        <input
+          type="number"
+          style={{ maxWidth: 40, border: '1px solid purple' }}
+          ref={monthRef}
+          value={month}
+          onChange={(e) => {
+            if (+e.target.value < 1 || +e.target.value > 12) {
+              setMonth(1)
+            } else {
+              setMonth(+e.target.value)
+            }
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Backspace' && +e.target.value < 10) {
+              setMonth('')
+            }
+            if (e.key === 'ArrowDown' && +e.target.value === 1) {
+              setMonth(13)
+            }
+          }}
+          onKeyUp={(e) => {
+            if (+e.key > 1 && +e.target.value > 1) {
+              yearRef.current.select()
+            }
+            if (
+              e.target.value.length > 1 &&
+              e.key !== 'ArrowDown' &&
+              e.key !== 'ArrowUp' &&
+              e.key !== 'ArrowLeft'
+            ) {
+              yearRef.current.select()
+            }
+            if (e.key === 'ArrowRight') {
+              yearRef.current.select()
+            }
+            if (e.key === 'ArrowLeft') {
+              dayRef.current.select()
+            }
+          }}
+          onFocus={() => setFocusField('monthFocus')}
+          onBlur={() => setFocusField('')}
+        />
+        <input
+          type="number"
+          style={{ maxWidth: 40, border: '1px solid green' }}
+          ref={yearRef}
+          value={year}
+          onChange={(e) => setYear(+e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Backspace' && e.target.value.length === 1) {
+              setYear('')
+            }
+          }}
+          onKeyUp={(e) => {
+            if (e.key === 'ArrowLeft') {
+              monthRef.current.select()
+            }
+          }}
+          onFocus={() => setFocusField('yearFocus')}
+          onBlur={() => setFocusField('')}
+        />
+      </styled.div>
 
       <styled.div
         style={{
--- src/components/Date/index.tsx
+++ src/components/Date/index.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react'
-import { styled, Style, StateProvider } from '~'
+import React, { FC, useState } from 'react'
+import { styled, Style } from '~'
 import { InputDate } from './InputDate'
 import { NewDateInput } from './NewDateInput'
 
@@ -15,41 +15,44 @@ type DateWidgetProps = {
 // TODO: utc input
 
 export const DateWidget: FC<DateWidgetProps> = ({ value, onChange, style }) => {
+  // only want one source of truth in milliseconds value
+  // TODO deze waarde moet zodalijk aan de onChange gereturned worden
+  const [millisecondsValue, setMilliSecondsValue] = useState(value)
+
   // van string format naar milliseconds
-  const onChangeHandler = (
-    str: string,
-    timeInput: string = '00:00'
-  ): number => {
-    const dateString = `${str?.split('/').reverse().join('-')}T${timeInput}`
-    const outputMs = new Date(dateString).getTime()
-    console.log('output in ms ⏱', outputMs)
-    onChange(outputMs)
-    return outputMs
-  }
+  // const onChangeHandler = (
+  //   str: string,
+  //   timeInput: string = '00:00'
+  // ): number => {
+  //   const dateString = `${str?.split('/').reverse().join('-')}T${timeInput}`
+  //   const outputMs = new Date(dateString).getTime()
+  //   console.log('output in ms ⏱', outputMs)
+  //   onChange(outputMs)
+  //   return outputMs
+  // }
 
-  const MscToString = (value: number): string => {
-    const newDate = new Date(value)
-    const year = newDate.getFullYear()
-    const month =
-      newDate.getMonth() + 1 < 10
-        ? '0' + (newDate.getMonth() + 1)
-        : newDate.getMonth() + 1
-    const day =
-      newDate.getDate() + 1 < 10
-        ? '0' + (newDate.getDate() + 1)
-        : newDate.getDate() + 1
+  // const MscToString = (value: number): string => {
+  //   const newDate = new Date(value)
+  //   const year = newDate.getFullYear()
+  //   const month =
+  //     newDate.getMonth() + 1 < 10
+  //       ? '0' + (newDate.getMonth() + 1)
+  //       : newDate.getMonth() + 1
+  //   const day =
+  //     newDate.getDate() + 1 < 10
+  //       ? '0' + (newDate.getDate() + 1)
+  //       : newDate.getDate() + 1
 
-    return `${day}/${month}/${year}`
-  }
+  //   return `${day}/${month}/${year}`
+  // }
 
-  const stringValue = MscToString(value)
+  // const stringValue = MscToString(value)
 
   return (
     <styled.div style={{ ...style }}>
-      <StateProvider values={{ value: stringValue }}>
-        <NewDateInput />
-        <InputDate value={value} onChangeHandler={onChangeHandler} />
-      </StateProvider>
+      <NewDateInput value={millisecondsValue} setValue={setMilliSecondsValue} />
+      top component millesconds: {millisecondsValue}
+      {/* <InputDate value={value} onChangeHandler={onChangeHandler} /> */}
     </styled.div>
   )
 }
