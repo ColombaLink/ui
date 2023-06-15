eb74ccf732816d0e91f0caa68da55cea785ea6b3
yves <misterybeer@gmail.com>
2023-06-15 10:00:20 +0200

    comment out for ubild

--- src/apps/Content/ContentMain/CustomContent.tsx
+++ src/apps/Content/ContentMain/CustomContent.tsx
@@ -17,68 +17,68 @@ import { BasedClient } from '@based/client'
 
 const parseFunction = () => {}
 
-const propsWalker = (
-  obj: any,
-  ctx: { data: any; state: any; args: any[]; client: BasedClient }
-): any => {
-  if (typeof obj !== 'object') {
-    return {}
-  }
+// const propsWalker = (
+//   obj: any,
+//   ctx: { data: any; state: any; args: any[]; client: BasedClient }
+// ): any => {
+//   if (typeof obj !== 'object') {
+//     return {}
+//   }
 
-  const newObj: any = Array.isArray(obj) ? [] : {}
+//   const newObj: any = Array.isArray(obj) ? [] : {}
 
-  for (const key in obj) {
-    const field = obj[key]
+//   for (const key in obj) {
+//     const field = obj[key]
 
-    if (/^on[A-Z]([a-z])+/.test(key)) {
-      if (typeof field === 'object') {
-        if (field.type === 'function') {
-          newObj[key] = async (...args) => {
-            const fn = propsWalker(field, {
-              data: ctx.data,
-              state: ctx.state,
-              args,
-              client: ctx.client,
-            })
-            return ctx.client.call(fn.name, fn.payload)
-          }
-        }
-      } else {
-        newObj[key] = () => console.error('Needs to be an object def')
-      }
-    } else if (typeof field === 'object') {
-      newObj[key] = propsWalker(field, ctx)
-    } else if (typeof field === 'string') {
-      if (field[0] === '$') {
-        // $data, $args, $state
-        const path = field.split('.')
-        const type = path[0]
-        if (type === '$data') {
-          let d = ctx.data
-          for (let i = 1; i < path.length; i++) {
-            const seg = path[i]
-            if (d?.[seg] !== undefined) {
-              d = d[seg]
-            } else {
-              d = undefined
-              break
-            }
-          }
-          newObj[key] = d
-        } else {
-          // lets add some args!
-          newObj[key] = 'latewr!!@#'
-        }
-      } else {
-        newObj[key] = field
-      }
-    } else {
-      newObj[key] = field
-    }
-  }
+//     if (/^on[A-Z]([a-z])+/.test(key)) {
+//       if (typeof field === 'object') {
+//         if (field.type === 'function') {
+//           newObj[key] = async (...args) => {
+//             const fn = propsWalker(field, {
+//               data: ctx.data,
+//               state: ctx.state,
+//               args,
+//               client: ctx.client,
+//             })
+//             return ctx.client.call(fn.name, fn.payload)
+//           }
+//         }
+//       } else {
+//         newObj[key] = () => console.error('Needs to be an object def')
+//       }
+//     } else if (typeof field === 'object') {
+//       newObj[key] = propsWalker(field, ctx)
+//     } else if (typeof field === 'string') {
+//       if (field[0] === '$') {
+//         // $data, $args, $state
+//         const path = field.split('.')
+//         const type = path[0]
+//         if (type === '$data') {
+//           let d = ctx.data
+//           for (let i = 1; i < path.length; i++) {
+//             const seg = path[i]
+//             if (d?.[seg] !== undefined) {
+//               d = d[seg]
+//             } else {
+//               d = undefined
+//               break
+//             }
+//           }
+//           newObj[key] = d
+//         } else {
+//           // lets add some args!
+//           newObj[key] = 'latewr!!@#'
+//         }
+//       } else {
+//         newObj[key] = field
+//       }
+//     } else {
+//       newObj[key] = field
+//     }
+//   }
 
-  return newObj
-}
+//   return newObj
+// }
 
 export const CustomContent = ({ view, actions }) => {
   const contextMenu = useContextMenu<{ view }>(actions, { view })
@@ -98,9 +98,9 @@ export const CustomContent = ({ view, actions }) => {
 
   const client = useClient()
 
-  const props = propsWalker(view.config.props ?? {}, { data, state, client })
+  // const props = propsWalker(view.config.props ?? {}, { data, state, client })
 
-  console.log('---------------->', props, data)
+  // console.log('---------------->', props, data)
 
   return (
     <ScrollArea
